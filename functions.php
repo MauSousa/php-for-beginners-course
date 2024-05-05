a5f6f19dc48737eff2862f63d8426b66ed1203c9
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-05 09:51:57 -0600

    Add own router related file

--- functions.php
+++ functions.php
@@ -22,19 +22,3 @@ function dede($text)
     var_export($text);
     echo '</pre>';
 }
-
-function abort($code = 404)
-{
-    http_response_code($code);
-    require "views/{$code}.view.php";
-    die();
-}
-
-function routeToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort();
-    }
-}

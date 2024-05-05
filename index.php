c2bd32e05d25be22a5412e9ad5fc7e2f1884f139
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-05 09:24:26 -0600

    Add basic router

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 
 require "functions.php";
 
-$title = "Home";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-require "views/index.view.php";
+// if ($uri === '/') {
+//     require 'controllers/index.php';
+// } else if ($uri === '/about') {
+//     require 'controllers/about.php';
+// } else if ($uri === '/contact') {
+//     require 'controllers/contact.php';
+// }
+
+$routes = [
+    '/'         => 'controllers/index.php',
+    '/about'    => 'controllers/about.php',
+    '/contact'  => 'controllers/contact.php',
+];
+
+if (array_key_exists($uri, $routes)) {
+    require $routes[$uri];
+} else {
+    http_response_code(404);
+    $title = '404 Not Found';
+    require "views/404.view.php";
+    die();
+}

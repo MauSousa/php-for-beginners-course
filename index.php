82611fd760a3df7527ce0c4c4e5a5f18eac25d96
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-05 09:52:15 -0600

    Add router require

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
 
 require "functions.php";
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/'         => 'controllers/index.php',
-    '/about'    => 'controllers/about.php',
-    '/contact'  => 'controllers/contact.php',
-];
-
-routeToController($uri, $routes);
+require "router.php";

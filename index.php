466cd37835884d2eca2d9951099c8e61c59ef976
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-05 09:47:24 -0600

    Add router function to index

--- index.php
+++ index.php
@@ -4,25 +4,10 @@ require "functions.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-// if ($uri === '/') {
-//     require 'controllers/index.php';
-// } else if ($uri === '/about') {
-//     require 'controllers/about.php';
-// } else if ($uri === '/contact') {
-//     require 'controllers/contact.php';
-// }
-
 $routes = [
     '/'         => 'controllers/index.php',
     '/about'    => 'controllers/about.php',
     '/contact'  => 'controllers/contact.php',
 ];
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-    $title = '404 Not Found';
-    require "views/404.view.php";
-    die();
-}
+routeToController($uri, $routes);

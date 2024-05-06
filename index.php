ca60c31a16d9d62d80aed1832079fa953804f945
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-06 17:27:51 -0600

    Add pdo connection

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
+
+// MySQL connection
+$dsn = "mysql:host=127.0.01;port=3310;dbname=myapp;charset=utf8mb4";
+$options = [
+    PDO::ATTR_EMULATE_PREPARES   => false, // Disable emulation mode for "real" prepared statements
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Disable errors in the form of exceptions
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Make the default fetch be an associative array
+];
+$pdo = new PDO($dsn, "laracasts", "root_root", $options);
+
+$query = "select * from posts";
+$statement = $pdo->prepare($query);
+
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}

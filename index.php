df0e3e45b83ea4b58998e3626c0cf5cbe2890d0d
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-07 17:17:15 -0600

    Add router to start notes project

--- index.php
+++ index.php
@@ -5,12 +5,6 @@ declare(strict_types=1);
 require __DIR__ . '/config/constants.php';
 require DIR_BASE . '/classes/Database.class.php';
 require "functions.php";
-// require "router.php";
+require "router.php";
 
-$config = require(DIR_BASE . '/config/config.php');
-$db = new Database($config);
 
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, ['id' => $id])->fetch();

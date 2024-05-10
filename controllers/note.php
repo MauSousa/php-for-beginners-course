bd6a650a82ed89b9cf528e19b1eca17acccc4a87
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-10 09:50:54 -0600

    Add intro to authorization

--- controllers/note.php
+++ controllers/note.php
@@ -6,6 +6,10 @@ $db = new Database($config);
 $title = 'Note';
 
 $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();
-// dd($note);
+
+if (!$note) abort();
+
+$currentUserId = 1;
+if ($note['user_id'] !== $currentUserId) abort(Response::FORBIDDEN);
 
 require("views/note.view.php");

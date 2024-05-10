af2d4d41fbb2c300aca8268a7f827c86ba43958b
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-10 09:49:04 -0600

    Add response error class

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -31,8 +31,6 @@ class Database
         $statement = $this->connection->prepare($query);
         $statement->execute($params);
 
-        error_log($query);
-
         return $statement;
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
 require __DIR__ . '/config/constants.php';
 require DIR_BASE . '/classes/Database.class.php';
+require DIR_BASE . '/classes/Response.class.php';
 require "functions.php";
 require "router.php";
-
-

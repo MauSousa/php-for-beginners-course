bd99a125deb2cf3d93098c75684df938e7b66976
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-13 13:06:46 -0600

    Add basic server validation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,22 @@ $db = new Database($config['database']);
 $title = 'Create a note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT notes SET body = :body, user_id = :user_id', [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+
+    $errors = [];
+    if (strlen($_POST['body']) === NULL_CHAR_NOTE) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if (strlen($_POST['body']) > MAX_CHAR_NOTE) {
+        $errors['body'] = "The body of the note can not be more than 1,000 characters";
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT notes SET body = :body, user_id = :user_id', [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require('views/note-create.view.php');

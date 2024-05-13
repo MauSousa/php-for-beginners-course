47c67f67f24b297648ec4b623ad25b002c044a23
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-13 11:45:46 -0600

    Add query to save data into the db

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,5 +1,15 @@
 <?php
 
+$config = require(DIR_BASE . '/config/config.php');
+$db = new Database($config['database']);
+
 $title = 'Create a note';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $db->query('INSERT notes SET body = :body, user_id = :user_id', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
+}
 
 require('views/note-create.view.php');

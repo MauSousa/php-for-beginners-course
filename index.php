2e08b23b289b5ac746dd5ebc516857911e87edd4
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-14 11:29:41 -0600

    Add public directory to store index page

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/config/constants.php';
-require DIR_BASE . '/classes/Database.class.php';
-require DIR_BASE . '/classes/Response.class.php';
-require "functions.php";
-require "router.php";
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . "functions.php";
+
+require base_path('constants.php');
+require base_path('classes/Database.class.php');
+require base_path('classes/Response.class.php');
+require base_path('router.php');

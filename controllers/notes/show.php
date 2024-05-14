8a0c2206c4a7e8f2a37897e5252e4f749af087f7
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-14 15:43:32 -0600

    Add logic to handle delete

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,13 +4,27 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
-
-$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
-
 $currentUserId = 1;
-authorize($note['user_id'] === $currentUserId);
 
-view("notes/show.view.php", [
-    'title' => 'Note',
-    'note' => $note
-]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // form was submitted, delete the note
+    $note = $db->query("select * from notes where id = :id", ['id' => $_POST['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query("delete from notes where id = :id", [
+        'id' => $_POST['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'title' => 'Note',
+        'note' => $note
+    ]);
+}

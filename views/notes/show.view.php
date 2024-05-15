0c0513c831de5a6839b0cd64172fbcb34837954c
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-14 21:14:22 -0600

    Add new routes to the project

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,6 +10,7 @@
         <p><?= htmlspecialchars($note['body']) ?></p>
 
         <form class="mt-6" method="POST">
+            <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" value="<?= $note['id'] ?>" name="id">
             <button type="submit" class="text-sm text-red-500">Delete</button>
         </form>

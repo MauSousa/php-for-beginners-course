249a069c506a586b13f860394bb008b1145e5fad
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-16 12:36:58 -0600

    Change db config for service container

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,9 +1,9 @@
 <?php
 
 use Core\Database;
+use Core\App;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
 $currentUserId = 1;
 
 $note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
--- controllers/notes/store.php
+++ controllers/notes/store.php
@@ -1,10 +1,11 @@
 <?php
 
+use Core\App;
 use Core\Database;
 use Core\Validator;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+$db = App::resolve(Database::class);
+
 $errors = [];
 
 if (!Validator::string($_POST['body'], MIN_CHAR_NOTE, MAX_CHAR_NOTE)) $errors['body'] = 'A body of no more than 1,000 characters is required';

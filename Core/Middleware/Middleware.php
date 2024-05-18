c3cdc05cb8035cbc93de29fe3c57ab55374138cb
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-17 20:03:24 -0600

    Separate logic into each file

--- Core/Middleware/Middleware.php
+++ Core/Middleware/Middleware.php
@@ -1,11 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Core\Middleware;
 
 class Middleware
 {
-    const MAP = [
+    protected const MAP = [
         'guest' => Guest::class,
         'auth' => Auth::class
     ];
+
+    public static function resolve(string|null $key)
+    {
+        // if no middleware is applied
+        if (!$key) return;
+
+        // checks if key exists, else false
+        $middleware = static::MAP[$key] ?? false;
+
+        // if middleware is not on middlewares array
+        if (!$middleware) {
+            throw new \Exception("No matching middleware found for key '{$key}' ");
+        }
+
+        (new $middleware)->handle();
+    }
 }

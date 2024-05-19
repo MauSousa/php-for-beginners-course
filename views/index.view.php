3367fe613ccadb7defde04b7e5eb8cbabe055e80
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-19 12:09:34 -0600

    Add user email to check if session is active

--- views/index.view.php
+++ views/index.view.php
@@ -4,8 +4,7 @@
 
 <main>
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello. Welcome to the home page</p>
-        <!-- Your content -->
+        <p>Hello, <?= $_SESSION['user']['email'] ?? 'Guest' ?> welcome to the home page</p>
     </div>
 </main>
 <?php require("partials/footer.php"); ?>

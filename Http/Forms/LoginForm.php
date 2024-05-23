076f0b6187e2afb59f405aa875dc4a18a8a6a2ce
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-22 18:14:19 -0600

    Add new way to throw exceptions

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -4,24 +4,40 @@ declare(strict_types=1);
 
 namespace Http\Forms;
 
+use Core\ValidationException;
 use Core\Validator;
 
 class LoginForm
 {
     private $errors = [];
 
-    public function validate(string $email, string $password)
+    public function __construct(public array $attributes)
     {
         // validate form inputs
-        if (!Validator::email($email)) {
+        if (!Validator::email($attributes['email'])) {
             $this->errors['email'] = 'Please provide a valid email address';
         }
 
-        if (!Validator::string($password)) {
+        if (!Validator::string($attributes['password'])) {
             $this->errors['password'] = 'Please provide a valid password';
         }
+    }
 
-        return empty($this->errors);
+    public static function validate(array $attributes)
+    {
+        $instance = new static($attributes);
+
+        return $instance->failed() ? $instance->throw() : $instance;
+    }
+
+    public function throw()
+    {
+        ValidationException::throw($this->errors(), $this->attributes);
+    }
+
+    public function failed()
+    {
+        return count($this->errors);
     }
 
     public function errors()
@@ -32,5 +48,7 @@ class LoginForm
     public function error(string $field, string $message)
     {
         $this->errors[$field] = $message;
+
+        return $this;
     }
 }

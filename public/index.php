44494e0df3088e08d4afaba3686b99430b0ae063
Augusto Sousa <lagrange.tesla@gmail.com>
2024-05-22 18:44:32 -0600

    Add composer to the project

--- public/index.php
+++ public/index.php
@@ -3,16 +3,15 @@
 use Core\Session;
 use Core\ValidationException;
 
+const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . '/vendor/autoload.php';
+
 session_name('notes-app');
 session_start();
 
-const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . "Core/functions.php";
 
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
 
 require base_path('constants.php');
 
